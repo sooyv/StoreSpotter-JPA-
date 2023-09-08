b79fc2a761bfe09c5758541d2bc72af878325e21
sooyv <2cow0.0l@gmail.com>
2023-09-08 17:32:49 +0900

    StoreSpotter build

---
+++ src/main/java/com/sojoo/StoreSpotter/StoreSpotterApplication.java
@@ -0,0 +1,13 @@
+package com.sojoo.StoreSpotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSpotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSpotterApplication.class, args);
+	}
+
+}
