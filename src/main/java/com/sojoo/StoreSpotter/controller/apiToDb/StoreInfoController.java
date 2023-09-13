8a13c85b3d004ce4be6bde8bbf74ad65bab760f6
주용현 <say06151@naver.com>
2023-09-13 20:16:23 +0900

    v2.resources directory sort

---
+++ src/main/java/com/sojoo/StoreSpotter/controller/apiToDb/StoreInfoController.java
@@ -0,0 +1,25 @@
+package com.sojoo.StoreSpotter.controller.apiToDb;
+
+
+import com.sojoo.StoreSpotter.service.apiToDb.StoreInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StoreInfoController {
+    private final StoreInfoService storeInfoService;
+
+    @Autowired
+    public StoreInfoController(StoreInfoService storeInfoService) {
+        this.storeInfoService = storeInfoService;
+    }
+
+    @PostMapping("/saveStoreInfo")
+    public void saveStoreInfo() {
+        storeInfoService.saveStoreInfoFromApiResponse();
+    }
+}
