3c38f38fd354f128cb18f25a1f8e735b7be7d3a6
sooyv <2cow0.0l@gmail.com>
2023-09-28 23:22:34 +0900

    git pull

--- src/main/java/com/sojoo/StoreSpotter/dao/apiToDb/StoreInfoMapper.java
+++ src/main/java/com/sojoo/StoreSpotter/dao/apiToDb/StoreInfoMapper.java
@@ -4,7 +4,7 @@ import com.sojoo.StoreSpotter.dto.apiToDb.StoreInfoDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.springframework.stereotype.Repository;
 
-@Repository()
+@Repository
 public interface StoreInfoMapper {
     @Insert("INSERT INTO store_info (bizesId, bizesNm, rdnmAdr) VALUES (#{bizesId}, #{bizesNm}, #{rdnmAdr})")
     void insertStoreInfo(StoreInfoDTO storeInfoDTO);
