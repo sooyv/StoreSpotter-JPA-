f5c2004e28d2ae0ffbdea2234d6232b2f2b8d239
주용현 <say06151@naver.com>
2023-12-13 18:59:09 +0900

    test commit.1

--- src/main/java/com/sojoo/StoreSpotter/controller/admin/AdminController.java
+++ src/main/java/com/sojoo/StoreSpotter/controller/admin/AdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-
+//
 @RestController
 public class AdminController {
 
