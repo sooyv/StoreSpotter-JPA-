b78199269ec9304c7d48e9ccf40b3362ed04bb31
주용현 <say06151@naver.com>
2024-03-09 14:56:18 +0900

    docker local-redis settings  -redisTest.v2

---
+++ src/main/java/com/sojoo/StoreSpotter/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package com.sojoo.StoreSpotter.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/************
+ * @info : Redis Repository Config 클래스
+ * @name : RedisRepositoryConfig
+ * @version : 1.0.0
+ * @Description : Lettuce 사용(비동기 요청 처리), RedisRepository 방식.
+ ************/
+@Configuration
+@RequiredArgsConstructor
+@EnableRedisRepositories
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    // lettuce
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    // Redis template
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());   //connection
+        redisTemplate.setKeySerializer(new StringRedisSerializer());    // key
+        redisTemplate.setValueSerializer(new StringRedisSerializer());  // value
+        return redisTemplate;
+    }
+}
